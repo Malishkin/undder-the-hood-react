6fd6b5c968be7d566cfbd352731f859c228fe0de
SvetaLantsman <svetlana@curalife.com>
2023-05-14 15:39:51 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Button from './components/UI/Button';
+import ParagpaphOutput from './components/Output/ParagraphOutout';
 
 function App() {
   const [isParagraphShown, setIsParagraphShown] = useState(false);
@@ -9,7 +10,7 @@ function App() {
   return (
     <div  className="app">
      <h1>React under the Hood</h1>
-     {isParagraphShown && <p>New Paragraph</p>}
+     <ParagpaphOutput isShown={isParagraphShown}/>
      <Button onClick={toogleParagraph}>Toggle Paragraph</Button>
     
     </div>
---
+++ src/components/Output/ParagraphOutout.js
@@ -0,0 +1,9 @@
+
+
+const ParagpaphOutput = (props) => {
+    return (
+        <p>{props.isShown ? 'New Paragraph': ''}</p>
+    );
+};
+
+export default ParagpaphOutput;
